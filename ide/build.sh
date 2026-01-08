0509125c67eabcbc70cc205c4d38334ff38902a5
dangtong76 <dangtong@gmail.com>
2026-01-08 21:28:35 +0900

    init

--- ide/build.sh
+++ ide/build.sh
@@ -10,3 +10,8 @@ docker build -t dangtong76/cloud-cicd-ide .
 # Create the builder
 docker buildx build  --builder multi-builder --platform linux/amd64,linux/arm64  -t dangtong76/cloud-cicd --push .
 docker buildx build  --platform linux/amd64,linux/arm64  -t dangtong76/cloud-cicd --push .
+
+# Seperate Build and merge to latest Tag
+docker buildx build -f Dockerfile.amd64 --platform linux/amd64 -t dangtong76/cloud-cicd-ide:amd64 --push .
+docker buildx build -f Dockerfile.arm64 --platform linux/arm64 -t dangtong76/cloud-cicd-ide:arm64 --push .
+docker buildx imagetools create  -t dangtong76/cloud-cicd-ide:latest dangtong76/cloud-cicd-ide:amd64 dangtong76/cloud-cicd-ide:arm64
